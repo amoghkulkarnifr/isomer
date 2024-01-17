80756072d3d8d04cdf8bee7534fa2c604dba8899
amoghkulkarnifr <amogh.kulkarni@fradium.com>
2024-01-18 02:47:28 +0530

    Views added with a test component

---
+++ packages/views/src/components/TestComponent/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FC } from "react";
+import { View, Text } from "tamagui";
+
+type TestComponentProps = {};
+
+export const TestComponent: FC<TestComponentProps> = () => {
+  return (
+    <>
+      <View>
+        <Text>Hello from a Tamagui component!</Text>
+      </View>
+    </>
+  );
+};
